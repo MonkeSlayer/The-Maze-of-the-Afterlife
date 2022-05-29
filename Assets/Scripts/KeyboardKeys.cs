8e820865722e842e3673a4248d767f158286d76f
MonkeSlayer <panu.muhammadali@gmail.com>
2022-05-29 10:23:21 -0600

    Forgot to add Escape Key!

---
+++ Assets/Scripts/KeyboardKeys.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardKeys : MonoBehaviour
+{
+    void Update()
+    {
+        ProcessKeyboardKeys();
+    }
+
+    void ProcessKeyboardKeys()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
